6bd19ee9525a7d4044ecd03bd9d7e3cf7290df2a
Davin Lewis <102100872+davinlewis@users.noreply.github.com>
2023-09-10 13:08:58 -0700

    mainmenu stuff

--- UDC Galf/Assets/Scripts/GameManager.cs
+++ UDC Galf/Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -21,7 +23,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if(Input.GetKeyDown(KeyCode.Escape))
+            {
+            SceneManager.LoadScene("MainMenu");
+        }
     }
 
     public void ShowMenu(bool state) {
