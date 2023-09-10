0083e7a7220567cf4d44c1994be191d6baf6a5b3
Davin Lewis <102100872+davinlewis@users.noreply.github.com>
2023-09-10 00:18:15 -0700

    added levels 1-5 to level select

--- UDC Galf/Assets/Scripts/LevelScript.cs
+++ UDC Galf/Assets/Scripts/LevelScript.cs
@@ -9,4 +9,24 @@ public class LevelScript : MonoBehaviour
     {
         SceneManager.LoadScene("Level1");
     }
+
+    public void PlayLevel2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+
+    public void PlayLevel3()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+
+    public void PlayLevel4()
+    {
+        SceneManager.LoadScene("Level4");
+    }
+
+    public void PlayLevel5()
+    {
+        SceneManager.LoadScene("Level5");
+    }
 }
